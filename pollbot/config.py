89e97e69befaf90d06203a0bff382098f80f54ff
ahmed45682 <criminalfreefire948@gmail.com>
2025-05-02 09:45:51 +0300

    Update config.py

--- pollbot/config.py
+++ pollbot/config.py
@@ -8,7 +8,7 @@ import toml
 default_config = {
     "telegram": {
         "bot_name": "your_bot_@_username",
-        "api_key": "your_telegram_api_key",
+        "api_key": "7871391150:AAGlJNCI7HjbC4ZMo5lIf6py-Q-mwTsFCdM",
         "worker_count": 20,
         "admin": "nukesor",
         "allow_private_vote": False,
